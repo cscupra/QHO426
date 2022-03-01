34c75c590796f5d228469afd7824a3afbd11e984
Kosty Scupra <100381378+kostyscp@users.noreply.github.com>
2022-03-01 20:16:05 +0000

    Created a basic input/output program.

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,4 @@
+# Display a box print("##########") print("# #") print("# #") print("##########")
+whole_number = int(input())
+decimal_number = float(input())
+print("Your age is " + str(user_age))
---
+++ basics/input/user_input.py
@@ -0,0 +1,1 @@
+# Ask user to enter their name print("What is your name human?") name = input() print("It is nice to meet you human", name)
