47d885afea92095f3d202bcf2b22f0764cc84849
Kosty Scupra <100381378+kostyscp@users.noreply.github.com>
2022-03-03 19:31:28 +0000

    string_operators

---
+++ basics/input/string_operators.py
@@ -0,0 +1,12 @@
+print("Enter the number of lives")
+life = int(input())
+print("Enter the energy level")
+life = int(input())
+print("Enter the shield level")
+life = int(input())
+print("Health has been set")
+print("Lives: ", "\u2665"*life)
+print("Energy: ", "\u2666"*life)
+print("Shield: ", "\u2666"*life)
+
+
